fa753e7e3d138d6bf36b8612b85ae073436b6dd3
Erv16 <epalani1@binghamton.edu>
2020-08-23 11:35:45 -0400

    create Compensation data object

---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,43 @@
+package com.mindex.challenge.data;
+
+import java.time.LocalDate;
+
+public class Compensation {
+
+    private String employeeId;
+    private double salary;
+    private LocalDate effectiveDate;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(LocalDate effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Compensation{" +
+                "employeeId='" + employeeId + '\'' +
+                ", salary=" + salary +
+                ", effectiveDate=" + effectiveDate +
+                '}';
+    }
+}
