3adc6ccb98146a18caacd3ff3d93f93d39cd069d
Erv16 <epalani1@binghamton.edu>
2020-08-23 12:53:54 -0400

    refactor compensation service implementation to check if employee exists before creating or retrieving compensations else geenrate an exception

--- src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
@@ -4,9 +4,12 @@ import com.mindex.challenge.dao.CompensationRepository;
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.exception.EmployeeNotFoundException;
 import com.mindex.challenge.service.CompensationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CompensationServiceImpl implements CompensationService {
@@ -23,22 +26,21 @@ public class CompensationServiceImpl implements CompensationService {
     @Override
     public Compensation createCompensation(Compensation compensation) {
 
-        Employee employee = employeeRepository.findByEmployeeId(compensation.getEmployeeId());
-        if(employee == null) {
-            System.out.println("Employee doesn't exist");
+        Optional<Employee> employee = Optional.ofNullable(employeeRepository.findByEmployeeId(compensation.getEmployeeId()));
+        if(!employee.isPresent()) {
+            throw new EmployeeNotFoundException("Employee with employee id " + compensation.getEmployeeId() + " does not exist");
         }
-
         return compensationRepository.insert(compensation);
     }
 
     @Override
     public Compensation readCompensation(String employeeId) {
 
-        Compensation compensation = compensationRepository.findCompensationByEmployeeId(employeeId);
-        if(compensation == null) {
-            System.out.println("Could not find compensation as employee doesn't exist");
+        Optional<Employee> employee = Optional.ofNullable(employeeRepository.findByEmployeeId(employeeId));
+        if(!employee.isPresent()) {
+            throw new EmployeeNotFoundException("Employee with employee id " + employeeId + " does not exist");
         }
 
-        return compensation;
+        return compensationRepository.findCompensationByEmployeeId(employeeId);
     }
 }
