beafd8de2bff9d526bd759652139c68ec43b02ef
Erv16 <epalani1@binghamton.edu>
2020-08-23 12:06:10 -0400

    update Compensation Service to create and read compensations

--- src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
@@ -1,18 +1,42 @@
 package com.mindex.challenge.service.impl;
 
+import com.mindex.challenge.dao.CompensationRepository;
+import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.service.CompensationService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CompensationServiceImpl implements CompensationService {
 
+    private CompensationRepository compensationRepository;
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public CompensationServiceImpl(CompensationRepository compensationRepository, EmployeeRepository employeeRepository) {
+        this.compensationRepository = compensationRepository;
+        this.employeeRepository = employeeRepository;
+    }
 
     @Override
     public Compensation createCompensation(Compensation compensation) {
-        return null;
+
+        Employee employee = employeeRepository.findByEmployeeId(compensation.getEmployeeId());
+        if(employee == null) {
+            System.out.println("Employee doesn't exist");
+        }
+
+        return compensationRepository.insert(compensation);
     }
 
     @Override
     public Compensation readCompensation(String employeeId) {
-        return null;
+
+        Compensation compensation = compensationRepository.findCompensationByEmployeeId(employeeId);
+        if(compensation == null) {
+            System.out.println("Could not find compensation as employee doesn't exist");
+        }
+
+        return compensation;
     }
 }
