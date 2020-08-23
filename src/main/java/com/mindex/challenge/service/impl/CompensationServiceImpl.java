cb03d8b4bbf5ebcd168e6948580d613a805ab317
Erv16 <epalani1@binghamton.edu>
2020-08-23 11:50:28 -0400

    create Compensation service implementation boilerplate template

---
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
@@ -0,0 +1,18 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.service.CompensationService;
+
+public class CompensationServiceImpl implements CompensationService {
+
+
+    @Override
+    public Compensation createCompensation(Compensation compensation) {
+        return null;
+    }
+
+    @Override
+    public Compensation readCompensation(String employeeId) {
+        return null;
+    }
+}
