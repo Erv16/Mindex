facc6ab37030c48cf64d67a843c6d2e2ead4489a
Erv16 <epalani1@binghamton.edu>
2020-08-24 15:50:49 -0400

    add Swagger dependencies and generate swagger documentation for Compensation

--- src/main/java/com/mindex/challenge/controller/CompensationController.java
+++ src/main/java/com/mindex/challenge/controller/CompensationController.java
@@ -2,6 +2,10 @@ package com.mindex.challenge.controller;
 
 import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.service.CompensationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Api(description = "REST API calls pertaining to employee compensations")
 public class CompensationController {
 
     private static final Logger LOG = LoggerFactory.getLogger(CompensationController.class);
@@ -25,6 +30,11 @@ public class CompensationController {
     }
 
     @PostMapping("/compensation")
+    @ApiOperation(value = "Creates a compensation record for an existing employee", notes = "Returns the newly created compensation record of an employee")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
     public Compensation createCompensation(@RequestBody Compensation compensation) {
 
         LOG.debug("Received compensation create request for [{}]", compensation.getEmployeeId());
@@ -32,6 +42,11 @@ public class CompensationController {
     }
 
     @PutMapping("/compensation/{employeeId}")
+    @ApiOperation(value = "Updates a compensation record for an existing employee", notes = "Returns the updated compensation record of an employee")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
     public Compensation updateCompensation(@PathVariable String employeeId, @RequestBody Compensation compensation) {
 
         LOG.debug("Received compensation update request for [{}]", employeeId);
@@ -40,6 +55,11 @@ public class CompensationController {
     }
 
     @GetMapping("/compensation/{employeeId}")
+    @ApiOperation(value = "Retrieves the compensation record of an existing employee", notes = "Returns the compensation record of an employee")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
     public Compensation readCompensation(@PathVariable String employeeId) {
 
         LOG.debug("Received compensation read request for [{}]", employeeId);
