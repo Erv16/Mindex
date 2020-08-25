e7572a70d18a135143abeda46085743c00453fc2
Erv16 <epalani1@binghamton.edu>
2020-08-24 23:34:05 -0400

    update Reporting Structure service to calculate and return the number of direct reports

--- src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -1,12 +1,48 @@
 package com.mindex.challenge.service.impl;
 
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.exception.EmployeeNotFoundException;
 import com.mindex.challenge.service.ReportingStructureService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
 
 public class ReportingStructureServiceImpl implements ReportingStructureService {
 
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public ReportingStructureServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
     @Override
     public ReportingStructure generateNumberOfReports(String employeeId) {
-        return null;
+
+        ReportingStructure reportingStructure = new ReportingStructure();
+        Optional<Employee> employee = Optional.ofNullable(employeeRepository.findByEmployeeId(employeeId));
+        if(!employee.isPresent()) {
+            throw new EmployeeNotFoundException("Employee with employee id " + employeeId + " does not exist");
+        }
+        reportingStructure.setEmployee(employee.get());
+        reportingStructure.setNumberOfReports(numberOfReports(employee.get()));
+        return reportingStructure;
+    }
+
+    public int numberOfReports(Employee employee) {
+
+        int numberOfReportees = 0;
+        List<Employee> employeeDirectReports = employee.getDirectReports();
+        if(employeeDirectReports != null || employeeDirectReports.size() != 0) {
+            for(Employee reportee: employeeDirectReports) {
+                numberOfReportees += reportee.getDirectReports().size();
+                numberOfReportees++;
+            }
+        }
+
+        return numberOfReportees;
     }
 }
