75139d0d55af72fe24c755532d29089186990ea2
Erv16 <epalani1@binghamton.edu>
2020-08-24 23:20:45 -0400

    create the Reporting Structure service interface and boilerplate implementation

---
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -0,0 +1,12 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+
+public class ReportingStructureServiceImpl implements ReportingStructureService {
+
+    @Override
+    public ReportingStructure generateNumberOfReports(String employeeId) {
+        return null;
+    }
+}
